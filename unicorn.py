582b04b6b98320786f5745cba1674dcb5d4d5850
aiman badhrul <aimanbadhrul@gmail.com>
2022-04-02 14:55:05 +0800

    Update unicorn.py

--- unicorn.py
+++ unicorn.py
@@ -1,9 +1,21 @@
+import streamlit as st 
+import numpy as np 
 import pandas as pd
-import seaborn as sns
-from sklearn.linear_model import LinearRegression
-import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
+
+#import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.metrics import classification_report
+
 
 
 
