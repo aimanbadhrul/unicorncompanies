e795770d3f1c46fc563d69ff225b47ecfc3003d4
aiman badhrul <aimanbadhrul@gmail.com>
2022-04-02 14:54:32 +0800

    Update unicorn.py

--- unicorn.py
+++ unicorn.py
@@ -1,20 +1,9 @@
-import streamlit as st 
-import numpy as np 
 import pandas as pd
-
-#import matplotlib.pyplot as plt
-from matplotlib import pyplot as plt
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-
-from sklearn.metrics import classification_report
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 
@@ -37,5 +26,5 @@ choice = st.sidebar.selectbox(
 
 st.write(f"## Unicorn Companies from <font color='Aquamarine'>{choice}</font> ", unsafe_allow_html=True)
 
-unicorn_master = pd.read_csv("\Users\aiman\Documents\Singularity Aerotech Asia\Training\MIGHT Reskilling Cohort 3\Python\dataset\Unicorn_Companies.csv")
+unicorn_master = pd.read_csv(r"C:\Users\aiman\Documents\Singularity Aerotech Asia\Training\MIGHT Reskilling Cohort 3\Python\dataset\Unicorn_Companies.csv")
 unicorn_master.head(3)
