ff215fb087e24da3ec1da54084c9ed58785b7787
aiman badhrul <aimanbadhrul@gmail.com>
2022-04-02 17:09:22 +0800

    Update unicorn.py

--- unicorn.py
+++ unicorn.py
@@ -16,8 +16,8 @@ from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.metrics import classification_report
 
-
-
+unicorn_master = pd.read_csv('Unicorn_Companies.csv')
+unicorn_master = unicorn_master.drop(['City' , 'Select Inverstors' , 'Financial Stage'] , axis=1)
 
 st.title('Unicorn Companies Around the World')
 
@@ -27,12 +27,12 @@ Unicorn Companies Analysis App
 
 st.sidebar.write("<a href='https://www.linkedin.com/in/aimanbadhrul/'>Aiman Badhrulhisham </a>", unsafe_allow_html=True)
 
+choice = st.sidebar.selectbox(
+    "Choose a country",   
+    ('Cool','Not cool'),
+    index = 0)
 
 st.write(f"## Unicorn Companies from <font color='Aquamarine'>{choice}</font> ", unsafe_allow_html=True)
-
-
-unicorn_master = pd.read_csv('Unicorn_Companies.csv')
-unicorn_master = unicorn_master.drop(['City' , 'Select Inverstors' , 'Financial Stage'] , axis=1)
 
 st.write (unicorn_master.head(3))
 
@@ -40,9 +40,6 @@ st.write (f"Shape of the dataset is {unicorn_master.shape}")
 
 st.write (unicorn_master.describe())
 
-choice = st.sidebar.selectbox(
-    "Choose a country",   
-    ('Cool','Not cool'),
-    index = 0)
+
 
 
