00cf3dcad6b2f22d61b73e0b3537a5f837029e34
aiman badhrul <aimanbadhrul@gmail.com>
2022-04-02 17:06:30 +0800

    Update unicorn.py

--- unicorn.py
+++ unicorn.py
@@ -25,6 +25,14 @@ st.sidebar.write("""
 Unicorn Companies Analysis App
 """)
 
+choice = st.sidebar.selectbox(
+    "Choose a country",   
+    ('Cool','Not cool'),
+    index = 0
+    
+)
+
+st.write(f"## Unicorn Companies from <font color='Aquamarine'>{choice}</font> ", unsafe_allow_html=True)
 
 st.sidebar.write("<a href='https://www.linkedin.com/in/aimanbadhrul/'>Aiman Badhrulhisham </a>", unsafe_allow_html=True)
 
@@ -38,11 +46,4 @@ st.write (f"Shape of the dataset is {unicorn_master.shape}")
 
 st.write (unicorn_master.describe())
 
-choice = st.sidebar.selectbox(
-    "Choose a country",   
-    ('Cool','Not cool'),
-    index = 0
-    
-)
 
-st.write(f"## Unicorn Companies from <font color='Aquamarine'>{choice}</font> ", unsafe_allow_html=True)
